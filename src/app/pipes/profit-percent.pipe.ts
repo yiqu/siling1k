97988a942b9d8603657dff35bf2e7aa1f5f33039
Kevin <kevinqu88@gmail.com>
2018-11-16 07:22:51 -0500

    fixed decimal points to 2 dig

--- src/app/pipes/profit-percent.pipe.ts
+++ src/app/pipes/profit-percent.pipe.ts
@@ -4,7 +4,7 @@ import { Pipe, PipeTransform } from '@angular/core';
 export class ProfitPercentPipe implements PipeTransform {
   transform(value: number): string {
     let result;
-    let percent = value * 100;
+    let percent = (value * 100).toFixed(2);
     if (isNaN(percent)) {
       return "N/A";
     } else if (percent < 0) {
