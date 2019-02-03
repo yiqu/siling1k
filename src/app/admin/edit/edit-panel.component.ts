c4914193b8a23257f066608bb868e09907f4ff59
Kevin Qu <kevinqu88@gmail.com>
2019-02-03 18:34:36 -0500

    some stuff

--- src/app/admin/edit/edit-panel.component.ts
+++ src/app/admin/edit/edit-panel.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { AdminService } from 'src/app/service/admin.service';
+import { Router, ActivatedRoute, Data, Params } from '@angular/router';
 
 @Component({
   selector: 'admin-edit-panel',
@@ -9,15 +10,27 @@ import { AdminService } from 'src/app/service/admin.service';
 
 export class PanelEditComponent implements OnInit {
 
-  constructor(public as: AdminService) {
+  currentlyEditing: string;
 
+  constructor(public as: AdminService, public router: Router, 
+    public route: ActivatedRoute) {
   }
 
   ngOnInit() {
-
+    this.route.queryParams.subscribe(
+      (params: Params) => {
+        console.log(params);
+        if (params) {
+          this.currentlyEditing = params.editingId;
+        } else {
+          this.currentlyEditing = null;
+        }
+      }
+    )
   }
 
   navigateToSiling(silingId: string) {
-    console.log(silingId)
+    this.router.navigate(['./'], {relativeTo: this.route, 
+      queryParams: {editingId: silingId}});
   }
 }
